95b9fde1b8714396c86da6b1e4db570af7c9ccc1
Denis Yerokhin <130320429+DenisYerokhin@users.noreply.github.com>
2023-06-08 00:18:53 +0700

    Update botopen.py

--- botopen.py
+++ botopen.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+from datetime import datetime
+
 import vk_api
 from configuration import user_token
 from vk_api.exceptions import ApiError
@@ -5,59 +8,73 @@ from vk_api.exceptions import ApiError
 
 class VkTools:
 
-    def __init__(self, token):
-        self.exist_api = vk_api.VkApi(token=token)
+    def __init__(self, user_token):
+        self.exist_api = vk_api.VkApi(token=user_token)
+
+    def year_bdate(self, bdate):
+        year_user = bdate.split('.')[2]
+        now = datetime.now().year
+        return now - int(year_user)
 
     def get_profile_info(self, user_id):
 
         try:
-            info = self.exist_api.method('users.get',
-                                         {'user_id': user_id,
-                                          'fields': 'bdate,city,sex'
-                                          }
-                                         )
-        except ApiError:
-            return
+            info, = self.exist_api.method('users.get',
+                                          {'user_id': user_id,
+                                           'fields': 'bdate,city,sex'
+                                           }
+                                          )
+        except ApiError as e:
+            info = {}
+            print(f'error = {e}')
 
-        return info
+        result_final = {'name': (info['first_name'] + ' ' + info['last_name']) if
+                        'first_name' in info and 'last_name' in info else None,
+                        'sex': info.get('sex'),
+                        'city': info.get('city')['title'] if info.get('city') is not None else None,
+                        'year': self.year_bdate(info.get('bdate'))
+                        }
+        return result_final
 
-    def user_search(self, city_id, age_from, age_to, sex, offset=None):
+    def user_search(self, params, offset=0):
 
         try:
             profiles = self.exist_api.method('users.search',
-                                             {'city_id': city_id,
-                                              'age_from': age_from,
-                                              'age_to': age_to,
-                                              'sex': sex,
-                                              'count': 30,
-                                              'offset': offset
-                                              })
+                                             {
+                                                 'home_town': params['city'],
+                                                 'age_from': params['year'] - 3,
+                                                 'age_to': params['year'] + 3,
+                                                 'sex': 1 if params['sex'] == 2 else 2,
+                                                 'count': 10,
+                                                 'offset': offset,
+                                                 'has_photo': True
+                                             }
+                                             )
 
-        except ApiError:
-            return
+        except ApiError as e:
+            profiles = []
+            print(f'error = {e}')
 
-        profiles = profiles['items']
+        result_now = [{'name': item['first_name'] + item['last_name'],
+                       'id': item['id']
+                       } for item in profiles if item['is_closed'] is False
+                      ]
 
-        final_result = []
-        for profile in profiles:
-            if profile['is_closed'] == False:
-                final_result.append({'name': profile['first_name'] + ' ' + profile['last_name'],
-                                    'id': profile['id']
-                                    })
+        return result_now
 
-        return final_result
-
-    def photos_get(self, user_id):
-
-        photos = self.exist_api.method('photos.get',
-                                       {'album_id': 'profile',
-                                        'owner_id': user_id
-                                        }
-                                       )
+    def photos_get(self, id):
         try:
-            photos = photos['items']
-        except KeyError:
-            return
+            photos = self.exist_api.method('photos.get',
+                                           {'album_id': 'profile',
+                                            'owner_id': id,
+                                            'extended': 1
+                                            }
+                                           )
+        except ApiError as e:
+            photos = {}
+            print(f'error = {e}')
+
+        return photos
 
         final_result = []
         for num, photo in enumerate(photos):
@@ -74,5 +91,9 @@ class VkTools:
 
 if __name__ == '__main__':
     tools = VkTools(user_token)
-    photos = tools.photos_get()
-    print(photos)
+    params = tools.get_profile_info('user_id')
+    worksheets = tools.user_search(params)
+    worksheet = worksheets.pop()
+    photos = tools.photos_get(worksheet['id'])
+
+    pprint(worksheets)
